36b16e0af812a75b04315a06b60c4ad48ec534c1
mariesonko62@gmail.com <mariesonko62@gmail.com>
2018-04-18 22:31:29 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,11 @@
-# Write your code here.
+def line(katz_deli)
+  if katz_deli.empty?     
+    puts "The line is currently empty."
+  else
+    position = []
+    the_line.each_with_index { |person, number|
+      position.push( "#{number + 1}. #{person}") }
+      puts "The line is currently: " + position.join(" ") 
+    end 
+  end 
+  
